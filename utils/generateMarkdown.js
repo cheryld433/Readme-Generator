88a7d36cda40489a0a1842856b9d211c4ce266c1
Cheryl Daniels <cheryld433@gmail.com>
2020-05-08 20:38:30 -0400

    Update generateMarkdown.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,46 +1,47 @@
 const generateMarkdown = data => {
-  return JSON.stringify(data);
   return `
+# ${ data.title.toUpperCase()}
+
+![License: ${(data.lic) ? data.lic : 'None'}](https://img.shields.io/badge/License-${(data.lic) ? data.lic : 'None'}-brightgreen)
+
+## Description: 
+${ data.desc }
+
+## Table of Content:
+  * [Installation](#installation)
+
+  * [Usage](#usage)
+
+  * [License](#license)
+
+  * [Contributors](#contributors)
+
+  * [Tests](#tests)
+
+  * [Questions](#questions)
 
 
+## Installation:
+${data.install}
 
-  
-# ${ data.title } 
+## Usage:
+${data.usage}
 
+## Contributors:
+${data.cont}
 
+## Tests:
+${data.test}
 
-![License: ${(data.license) ? data.license: 'None'}](https://img.sheilds.io/badge/License-${(data.license) ? data.license: 'None'}-brightgreen)
-
-_#Repository by ${data.name}_
-
-
-__##${data.description}__
-
-
-_###Installation:_
-${data.dependencies}
-
-
-
-
-
-
-
-
-
-
-
-
+#### Questions:
+${data.questions}
 `;
-//// generate just like a read me file 
-
-//installation
-// usage
-// contributors
-//tests
-//questions
-// avatar
-
 }
 
-module.exports = generateMarkdown
+
+
+module.exports = generateMarkdown;
+
+
+
+
